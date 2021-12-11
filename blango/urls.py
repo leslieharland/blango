bb0f9fa11aa95ae5f8ec4f98ea72faae6335b7eb
Leslie Harland <leslieharland@outlook.com>
2021-12-11 22:39:12 +0000

    Finish DRF views

--- blango/urls.py
+++ blango/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
-    path("api/v1/", include("blango.api_urls")),
+    path("api/v1/", include("blog.api.urls")),
 ]
 from django.conf import settings
 print(f"Time zone: {settings.TIME_ZONE}")
